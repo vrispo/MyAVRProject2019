b34462b8e45e7edce9434ad3e6ca22e8c17f2e01
vrispo <veronica_rispo@hotmail.it>
2020-02-04 19:12:49 +0100

    Broadcast fix

--- UtilityDll/ExampleDll/utilityDll.cpp
+++ UtilityDll/ExampleDll/utilityDll.cpp
@@ -5,6 +5,17 @@
 
 //TestDLL.__AddFunction( C_PCHAR , "get_IPaddress");
 
+void int_to_bin_digit(unsigned int in, int count, int* out)
+{
+	/* assert: count <= sizeof(int)*CHAR_BIT */
+	unsigned int mask = 1U << (count - 1);
+	int i;
+	for (i = 0; i < count; i++) {
+		out[i] = (in & mask) ? 1 : 0;
+		in <<= 1;
+	}
+}
+
 extern "C" __declspec(dllexport) char * get_IPaddress(){
 	std::string line;
 	std::ifstream IPFile;
@@ -35,6 +46,58 @@ extern "C" __declspec(dllexport) char * get_IPaddress(){
 				}
 				to_ret[i] = '\0';
 
+				/*				char temp[40];
+
+				strcpy(temp, to_ret);
+				
+				i = 0;
+
+				while (to_ret[i] != '.') {
+					temp[i] = to_ret[i];
+					i++;
+				}
+
+				int a[8];
+				unsigned int ip_1 = 0;
+			    ip_1 = atoi(temp);
+
+				int_to_bin_digit(ip_1, 8, a);
+
+				if (a[1] == 0) {
+					strcat(temp, ",B");
+				}
+				else{
+					strcat(temp, ",C");
+				}
+
+			std::cout << to_ret;
+
+				char * pch;
+				pch = strtok(to_ret, ".");
+				int ip_1 = atoi(pch);
+				int n;
+				uint8_t a[8];
+				for (int j = 0; ip_1 > 0; j++) {
+					a[j] = ip_1 % 2;
+					ip_1 = ip_1 / 2;
+				}
+
+				if (a[1] == 0) {
+					to_ret[i] = ',';
+					to_ret[i + 1] = 'B';
+					to_ret[i + 2] = '\0';
+				}else 
+					if (a[1] == 1) {
+						to_ret[i] = ',';
+						to_ret[i + 1] = 'C';
+						to_ret[i + 2] = '\0';
+					}
+					else {
+						to_ret[i] = ',';
+						to_ret[i + 1] = 'E';
+						to_ret[i + 2] = '\0';
+					}
+*/
 				return to_ret;			
 			}
 		}
